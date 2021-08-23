4e23a804effe5e4c56ff4ef2efe380c44bf94337
Seokseong Jeon <sjeon87+fork@gmail.com>
2021-08-23 23:23:25 +0900

    Make MemoryUsage a functional component

--- app/components/MemoryUsage.tsx
+++ app/components/MemoryUsage.tsx
@@ -1,6 +1,6 @@
 import { ipcRenderer } from 'electron';
 import { Progress } from 'semantic-ui-react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   CHAN_WORKER_TO_RENDERER,
   CHAN_RENDERER_TO_WORKER,
@@ -9,11 +9,6 @@ import {
   TYPE_MEMORY_USAGE_REQ,
   TYPE_MEMORY_USAGE,
 } from '../types';
-
-type State = {
-  avail: number;
-  used: number;
-};
 
 function b2mb(b: number) {
   return Math.ceil(b / 1024 / 1024);
@@ -27,43 +22,31 @@ function requestMemoryUsage() {
   });
 }
 
-export default class MemoryUsage extends React.Component<
-  Record<string, unknown>,
-  State
-> {
-  constructor(props: Record<string, unknown>) {
-    super(props);
-    this.state = {
-      avail: 100,
-      used: 1,
-    };
-    this.onIpc = this.onIpc.bind(this);
-  }
+export default function MemoryUsage() {
+  const [avail, setAvail] = useState(100);
+  const [used, setUsed] = useState(1);
 
-  componentDidMount() {
-    ipcRenderer.on(CHAN_WORKER_TO_RENDERER, this.onIpc);
+  useEffect(() => {
+    ipcRenderer.on(CHAN_WORKER_TO_RENDERER, onIpc);
     requestMemoryUsage();
-  }
-
-  componentWillUnmount() {
-    ipcRenderer.removeListener(CHAN_WORKER_TO_RENDERER, this.onIpc);
-  }
+    return function cleanup() {
+      ipcRenderer.removeListener(CHAN_WORKER_TO_RENDERER, onIpc);
+    }
+  }, []);
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  onIpc(_event: Electron.IpcRendererEvent, msg: any) {
+  function onIpc(_event: Electron.IpcRendererEvent, msg: any) {
     const { type } = msg;
     if (type === TYPE_MEMORY_USAGE) {
       const { avail, used } = msg;
-      this.setState({ avail, used });
+      setAvail(avail);
+      setUsed(used);
     }
   }
 
-  render() {
-    const { avail, used } = this.state;
-    return (
-      <Progress value={used} total={avail} size="tiny">
-        {`${b2mb(used)} / ${b2mb(avail)} MB used`}
-      </Progress>
-    );
-  }
+  return (
+    <Progress value={used} total={avail} size="tiny">
+      {`${b2mb(used)} / ${b2mb(avail)} MB used`}
+    </Progress>
+  );
 }
